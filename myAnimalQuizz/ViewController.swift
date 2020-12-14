566e6edc51bc533eb0d550f064cfeea2ff011dc5
DiuminPM <75372207+DiuminPM@users.noreply.github.com>
2020-12-14 12:00:53 +0300

    Initial Commit

---
+++ myAnimalQuizz/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  myAnimalQuizz
+//
+//  Created by DiuminPM on 14.12.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
